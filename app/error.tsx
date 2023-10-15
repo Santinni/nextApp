3e4e49c3d6e9ba0b3582218223f4776d8c70824c
Karel Kutchan <karel.kutchan@eman.cz>
2023-10-16 01:50:27 +0200

    Add: error page

---
+++ app/error.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+
+interface ErrorPageProps {
+  error: Error;
+  reset: () => void;
+}
+
+const ErrorPage = (props: ErrorPageProps) => {
+  const { error, reset } = props;
+  console.log(
+    "%cError: ",
+    "color: red; font-size: 18px; font-weight: bold;",
+    error
+  );
+  return (
+    <div className="flex flex-col w-full">
+      <h1 className="text-center">Error</h1>
+      <p className="text-center">
+        An unexpected error occurred. Please try again later.
+      </p>
+      <button className="btn btn-primary" onClick={() => reset()}>
+        Retry
+      </button>
+    </div>
+  );
+};
+
+export default ErrorPage;
