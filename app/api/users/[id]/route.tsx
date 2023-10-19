7f4a48a9158773a8cac3aa76624dda33ef79278a
Karel Kutchan <karel.kutchan@eman.cz>
2023-10-20 00:06:00 +0200

    Add: API, zod schema validation

---
+++ app/api/users/[id]/route.tsx
@@ -0,0 +1,58 @@
+import { NextRequest, NextResponse } from "next/server";
+import schema from "../schema";
+
+interface Props {
+  params: {
+    id: number;
+  };
+}
+
+export function GET(request: NextRequest, props: Props) {
+  //   return NextResponse.json({
+  //     message: "Hello World",
+  //   });
+  const { id } = props.params;
+  if (id > 10)
+    return NextResponse.json(
+      {
+        error: "User not found",
+      },
+      { status: 404 }
+    );
+  return NextResponse.json({
+    id,
+    name: "John Doe",
+    username: "johndoe",
+    email: "email",
+  });
+}
+
+export async function PUT(request: NextRequest, props: Props) {
+  const { id } = props.params;
+
+  const body = await request.json();
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  if (id > 10)
+    return NextResponse.json(
+      {
+        error: "User not found",
+      },
+      { status: 404 }
+    );
+  return NextResponse.json({ id, ...body });
+}
+
+export async function DELETE(request: NextRequest, props: Props) {
+  const { id } = props.params;
+
+  if (id > 10)
+    return NextResponse.json(
+      {
+        error: "User not found",
+      },
+      { status: 404 }
+    );
+  return NextResponse.json({ message: "User deleted" });
+}
