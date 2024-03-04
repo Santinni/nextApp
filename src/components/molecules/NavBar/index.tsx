2d45b4a758a612242e0ce1d4c9953eea8976804d
Karel Kutchan <karel.kutchan@email.cz>
2024-03-04 01:06:35 +0100

    rebase

--- src/components/molecules/NavBar/index.tsx
+++ src/components/molecules/NavBar/index.tsx
@@ -1,21 +1,44 @@
-import Link from "next/link"
+"use client"
 
-const NavBar = () => {
+import { FC } from "react"
+
+import clsx from "clsx"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+export interface NavBarProps {
+  navLinks: {
+    href: string
+    label: string
+  }[]
+  isVertical?: boolean
+  ariaLabel: string
+}
+
+const NavBar: FC<NavBarProps> = ({ navLinks, isVertical, ariaLabel }) => {
+  const pathname = usePathname()
+
   return (
-    <div className="flex bg-slate-200 p-5">
-      <Link href="/" className="mr-5">
-        Home
-      </Link>
-      <Link href="/users" className="mr-5">
-        Users
-      </Link>
-      <Link href="/products" className="mr-5">
-        Products
-      </Link>
-      <Link href="/admin" className="mr-5">
-        Admin
-      </Link>
-    </div>
+    <nav
+      aria-label={ariaLabel}
+      className={clsx("flex bg-slate-200 p-5 gap-5", isVertical && "flex-col")}
+    >
+      {navLinks.map((navLink) => {
+        const isActive =
+          navLink.href === "/"
+            ? pathname === navLink.href
+            : pathname.startsWith(navLink.href)
+        return (
+          <Link
+            key={navLink.href}
+            href={navLink.href}
+            className={isActive ? "font-bold" : undefined}
+          >
+            {navLink.label}
+          </Link>
+        )
+      })}
+    </nav>
   )
 }
 
