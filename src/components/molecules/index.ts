610386acaec60ccdc6fc4abb06f28e89273a9331
Karel Kutchan <karel.kutchan@email.cz>
2024-03-21 00:55:33 +0100

    Add: Next-Auth v5, google authentication, basic username authentication(hardcoded admin)

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,5 +1,6 @@
+import Header from "./Header"
 import NavBar from "./NavBar"
 import ProductCard from "./ProductCard"
 import UserTable from "./UserTable"
 
-export { NavBar, ProductCard, UserTable }
+export { Header, NavBar, ProductCard, UserTable }
